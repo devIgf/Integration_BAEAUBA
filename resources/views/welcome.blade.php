e5e831d89403b0c28ad4e68223b53c3b90e6ea1e
unknown <Administrateur@IGF.sn>
2024-08-09 12:59:10 +0000

    ajout du style pour l'attente lors de l'execution de la requete

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,16 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Integration</title>
     <link rel="stylesheet" href="../style/wel.css">
-    <style>
+    <link rel="stylesheet" href="../style/waiting.css">
 
-    </style>
 </head>
 <body>
     <div class="navbar">
         <div class="logo">
             <img src="../images/log.png" alt="Logo">
         </div>
-        <h1>Integration</h1>
+        <h1 style="color: red" >Integration</h1>
     </div>
 
     <div class="container">
@@ -33,14 +32,58 @@
         </div>
         @endif
 
-        <form action="{{ route('moi') }}" method="POST" enctype="multipart/form-data">
+        <form id="uploadForm" action="{{ route('moi') }}" method="POST" enctype="multipart/form-data">
             @csrf
             <div>
                 <label for="json_file">Upload JSON File:</label>
                 <input type="file" name="json_file" id="json_file" required>
             </div>
-            <button type="submit">Import</button>
+            <button type="submit" id="submitButton">Import</button>
         </form>
     </div>
+
+    <!-- Section de l'animation -->
+    <section id="loadingAnimation">
+        <div class="loader">
+            <span style="--i:1;"></span>
+            <span style="--i:2;"></span>
+            <span style="--i:3;"></span>
+            <span style="--i:4;"></span>
+            <span style="--i:5;"></span>
+            <span style="--i:6;"></span>
+            <span style="--i:7;"></span>
+            <span style="--i:8;"></span>
+            <span style="--i:9;"></span>
+            <span style="--i:10;"></span>
+            <span style="--i:11;"></span>
+            <span style="--i:12;"></span>
+            <span style="--i:13;"></span>
+            <span style="--i:14;"></span>
+            <span style="--i:15;"></span>
+            <span style="--i:16;"></span>
+            <span style="--i:17;"></span>
+            <span style="--i:18;"></span>
+            <span style="--i:19;"></span>
+            <span style="--i:20;"></span>
+        </div>
+    </section>
+
+    <script>
+        document.getElementById('uploadForm').addEventListener('submit', function() {
+            var submitButton = document.getElementById('submitButton');
+            var loadingAnimation = document.getElementById('loadingAnimation');
+
+            // Désactiver le bouton et afficher l'animation
+            submitButton.classList.add('disabled');
+            submitButton.disabled = true;
+            loadingAnimation.classList.add('active');
+        });
+
+        // Optionnel: arrêter l'animation une fois la requête terminée (côté serveur)
+        window.addEventListener('load', function() {
+            var loadingAnimation = document.getElementById('loadingAnimation');
+            loadingAnimation.classList.remove('active');
+        });
+    </script>
 </body>
 </html>
